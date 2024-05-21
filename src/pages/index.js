e1240861453bd61162822be086568380277f95bc
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 14:30:18 -0400

    added background image for hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className={styles.backgroundImage}></div>
       <div className={clsx('container', styles.heroBanner)}>
 
         <div className={styles.column}>
