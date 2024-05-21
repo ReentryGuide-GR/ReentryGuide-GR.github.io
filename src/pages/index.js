6385c4ade16caed1642a8d893b6d4aaf963ec94a
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 16:37:20 -0400

    added icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,36 +22,39 @@ function HomepageHeader() {
 
         <div className={styles.column}>
           {/* <img src="/img/logo2.png" alt="Logo" className={styles.logo} /> */}
-          <div className={styles.textContainer}>
+          <div className={styles.row}>
+            <img src="/img/logo.svg" alt="logo" className={styles.logo} />
+            <div className={styles.textContainer}>
 
-            <Heading as="h1" className={styles.hero__title}>
-              {siteConfig.title}
-            </Heading>
+              <Heading as="h1" className={styles.hero__title}>
+                {siteConfig.title}
+              </Heading>
 
-            <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
+              <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
 
-            <div className={styles.buttonContainer}>
-              <img src="/img/google-play.png" alt="Get it on Google Play" className={styles.googlePlay} />
+              <div className={styles.buttonContainer}>
+                <img src="/img/google-play.png" alt="Get it on Google Play" className={styles.googlePlay} />
+              </div>
+
+              <p className={styles.text}>Can't use a smart phone?</p>
+              <Link className={styles.iconButton} to="/docs/intro">
+                <div className={styles.buttonRow}>
+                  <img src="/img/download.png" className={styles.icon} alt="icon" />
+                  <span className={styles.iconButtonText}>Download Resource Sheet</span>
+                </div>
+                <img src="/img/arrow_forward.png" className={styles.arrow} alt="arrow" />
+              </Link>
+
+              <p className={styles.text}>Want to expand impact?</p>
+              <Link className={styles.iconButton} to="/docs/intro">
+                <div className={styles.buttonRow}>
+                  <img src="/img/article.png" className={styles.icon} alt="icon" />
+                  <span className={styles.iconButtonText}>View Documentation</span>
+                </div>
+                <img src="/img/arrow_forward.png" className={styles.arrow} alt="arrow" />
+              </Link>
+
             </div>
-
-            <p className={styles.text}>Can't use a smart phone?</p>
-            <Link className={styles.iconButton} to="/docs/intro">
-              <div className={styles.buttonRow}>
-                <img src="/img/download.png" className={styles.icon} alt="icon" />
-                <span className={styles.iconButtonText}>Download Resource Sheet</span>
-              </div>
-              <img src="/img/arrow_forward.png" className={styles.arrow} alt="arrow" />
-            </Link>
-
-            <p className={styles.text}>Want to expand impact?</p>
-            <Link className={styles.iconButton} to="/docs/intro">
-              <div className={styles.buttonRow}>
-                <img src="/img/article.png" className={styles.icon} alt="icon" />
-                <span className={styles.iconButtonText}>View Documentation</span>
-              </div>
-              <img src="/img/arrow_forward.png" className={styles.arrow} alt="arrow" />
-            </Link>
-
           </div>
         </div>
 
