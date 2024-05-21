7bca52bcc3ca0433eb42c16cdeccdbddae4cd7e3
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 18:33:16 -0400

    various changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,7 +130,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} by Reentry Reimagined. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
