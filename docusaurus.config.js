be3235e86e7a9fd01c60ab54202152cc09b0c8ed
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 14:53:59 -0400

    Added site title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '',
+        title: 'ReentryGuide GR',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
