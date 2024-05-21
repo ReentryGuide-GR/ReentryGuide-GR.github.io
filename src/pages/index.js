18c05e56a82dd9a693f04ffc4e39155847632fa4
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 09:26:28 -0400

    added illustration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+// index.js
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -13,22 +14,26 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.heroBanner)}>
 
+      <div className={styles.column}>
+        <div className={styles.illustrationContainer}>
+          <img src="/img/illustration.png" alt="Get it on Google Play" className={styles.illustration} />
+        </div>
+      </div>
+
+
         <div className={styles.column}>
-          <div className={styles.logoContainer}>
-            <img src="/img/logo2.png" alt="Logo" className={styles.logo} />
+          <div className={styles.row}>
+            {/* <img src="/img/logo2.png" alt="Logo" className={styles.logo} /> */}
             <div className={styles.textContainer}>
               <Heading as="h1" className={styles.hero__title}>
                 {siteConfig.title}
               </Heading>
               <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
-              <div className={styles.buttonContainer}>
-                <img src="/img/google-play.png" alt="Get it on Google Play" className={styles.googlePlay} />
-              </div>
             </div>
           </div>
-        </div>
-        
-        <div className={styles.column}>
+          <div className={styles.buttonContainer}>
+            <img src="/img/google-play.png" alt="Get it on Google Play" className={styles.googlePlay} />
+          </div>
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
