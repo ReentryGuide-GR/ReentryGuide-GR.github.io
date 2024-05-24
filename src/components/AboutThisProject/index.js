e0fc68ea10cc246d95472fd4a62b5004d340c5b0
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-23 23:13:19 -0400

    fix styling

--- src/components/AboutThisProject/index.js
+++ src/components/AboutThisProject/index.js
@@ -12,7 +12,8 @@ export default function AboutThisProject() {
             <p>Formerly incarcerated individuals often face significant challenges when reintegrating into society. With only 55% reporting any earnings during their first year out of prison, many find themselves homeless. Limited financial resources make it difficult to obtain basic necessities like food, healthcare, and hygiene products.</p>
           </div>
           <div className={clsx('col col--4', styles.column)}>
-            <p>Moreover, many who have spent years in prison have never used a smartphone and struggle with modern technology. This makes it nearly impossible for them to use a computer to find local resources, such as charities that provide essential services.</p>
+            <p>Moreover, many who have spent years in prison have never used a smartphone and struggle with modern technology. This makes it nearly impossible for them to use a computer to find local resources, such as charities that provide essential services. </p>
+            <p>To address these issues, we designed and developed this app with simplicity and ease of use in mind. </p>
           </div>
           <div className={clsx('col col--4', styles.column)}>
             <p>Our goal is to streamline the process of finding essential resources, such as food, shelter, hygiene, and healthcare, enabling users to easily navigate to the locations offering these services.</p>
