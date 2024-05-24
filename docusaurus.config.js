17e19b5347bcc0d11fe1fb5b7ab0e36a01c3ef88
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-23 23:30:07 -0400

    update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,7 +130,7 @@ const config = {
 
   stylesheets: [
     {
-      href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800;900&family=Red+Hat+Display:wght@400;500;600;700&display=swap',
+      href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700;800;900&family=Red+Hat+Display:wght@400;500;600;700&display=swap',
       type: 'text/css',
     },
   ],
