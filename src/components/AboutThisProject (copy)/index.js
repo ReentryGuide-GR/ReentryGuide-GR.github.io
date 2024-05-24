52750518a2cea95964b0bee941f641a8ad116299
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-24 11:31:11 -0400

    create 2 files, copy 1 file and update 1 file

--- src/components/AboutThisProject (copy)/index.js
+++ src/components/AboutThisProject (copy)/index.js
@@ -10,10 +10,12 @@ export default function AboutThisProject() {
         <div className="row">
           <div className={clsx('col col--4', styles.column)}>
             <p>Formerly incarcerated individuals often face significant challenges when reintegrating into society. With only 55% reporting any earnings during their first year out of prison, many find themselves homeless. Limited financial resources make it difficult to obtain basic necessities like food, healthcare, and hygiene products.</p>
-            <p>Moreover, many who have spent years in prison have never used a smartphone and struggle with modern technology. This makes it nearly impossible for them to use a computer to find local resources, such as charities that provide essential services. </p>
           </div>
           <div className={clsx('col col--4', styles.column)}>
+            <p>Moreover, many who have spent years in prison have never used a smartphone and struggle with modern technology. This makes it nearly impossible for them to use a computer to find local resources, such as charities that provide essential services. </p>
             <p>To address these issues, we designed and developed this app with simplicity and ease of use in mind. </p>
+          </div>
+          <div className={clsx('col col--4', styles.column)}>
             <p>Our goal is to streamline the process of finding essential resources, such as food, shelter, hygiene, and healthcare, enabling users to easily navigate to the locations offering these services.</p>
             <p>Currently, this app is only implemented in Grand Rapids. We encourage you to fork this app and implement the project in your city to help more people benefit from these essential resources.</p>
           </div>
